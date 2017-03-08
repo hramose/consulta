91e964786ead4bd40e2de6fbeb1e71e87ab757e6
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-03-07 22:24:56 -0600

    test schedule call

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,12 +27,24 @@ class Kernel extends ConsoleKernel
     {
        /* $schedule->command(\App\Console\Commands\ReminderAppointment::class)
                  ->everyFiveMinutes();*/
+                 
         $schedule->call(function () {
 
-                $exitCode = \Artisan::call('consulta:reminderAppointment');
+        
+                //url contra la que atacamos
+                $ch = curl_init("http://consulta.avotz.com/appointments/reminder");
+                //a true, obtendremos una respuesta de la url, en otro caso, 
+                //true si es correcto, false si no lo es
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                //establecemos el verbo http que queremos utilizar para la petición
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+                //enviamos el array data
+                //curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+                //obtenemos la respuesta
+                $response = curl_exec($ch);
+                // Se cierra el recurso CURL y se liberan los recursos del sistema
+                curl_close($ch);
 
-                return $exitCode;
-                
         })->everyFiveMinutes();
        
     }
