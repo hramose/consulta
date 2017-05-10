8527fbdc2de7480f64adba539c97701f9674ca71
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-05-10 01:44:29 -0600

    se creo encuestas para medicos

--- app/Poll.php
+++ app/Poll.php
@@ -7,12 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Poll extends Model
 {
     protected $fillable = [
-        'user_id','name'
+        'appointment_id','user_id','name'
     ];
 
     public function user() //medic
     {
         return $this->belongsTo(User::class);
+    }
+    public function appointment() 
+    {
+        return $this->belongsTo(Appointment::class);
     }
     public function questions() 
     {
