3e27f61bbb2a2408ea5d13329ab7ed33a2e21790
Alonso Avotz <alonso@avotz.com>
2017-06-20 17:53:30 -0600

    se agrego link para compartir

--- resources/views/home-admin.blade.php
+++ resources/views/home-admin.blade.php
@@ -5,6 +5,24 @@
 
     <section class="content">
         
+        <div class="box box-info">
+  			<div class="box-header with-border">
+  				Link para compartir
+  			</div>
+  			<div class="box-body">
+  				<label>Url de Registro de un médico</label>
+  				<div class="input-group">
+	                <span class="input-group-addon">@</span>
+	                <input type="text" class="form-control" placeholder="Url de Registro de un médico" readonly value="{{  url('medic/register') }}">
+	              </div>
+	              <br>
+	              <label>Url de Registro de una clínica y su administrador</label>
+	              <div class="input-group">
+	                <span class="input-group-addon">@</span>
+	                <input type="text" class="form-control" placeholder="Url de Registro de una clinica y su administrador" readonly value="{{  url('clinic/register') }}">
+	              </div>
+  			</div>
+  		</div>
 
     </section>
 
--- resources/views/layouts/partials/header-admin.blade.php
+++ resources/views/layouts/partials/header-admin.blade.php
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="/" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>C</b></span>
+      <span class="logo-mini"><b>G</b></span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>{{ config('app.name', 'Laravel') }}</b></span>
     </a>
