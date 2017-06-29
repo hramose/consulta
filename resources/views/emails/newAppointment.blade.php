91b5ecaa45057d78f79a4bdd1e4b6961c9cf03e6
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-06-28 21:11:16 -0600

    fix email new cita

--- resources/views/emails/newAppointment.blade.php
+++ resources/views/emails/newAppointment.blade.php
@@ -3,7 +3,7 @@
 
 Cita con el Dr. {{ $appointment->user->name }}
 
-- Fecha: {{ $appointment->date->toDateString() }} De: {{   Carbon\Carbon::parse($appointment->start)->ToTimeString(); }} a: {{   Carbon\Carbon::parse($appointment->end)->ToTimeString(); }}
+- Fecha: {{ $appointment->date->toDateString() }} De: {{   Carbon\Carbon::parse($appointment->start)->ToTimeString() }} a: {{   Carbon\Carbon::parse($appointment->end)->ToTimeString() }}
 - Paciente: {{ $appointment->patient->first_name }} {{ $appointment->patient->last_name }}
 
 
