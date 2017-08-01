4d768971fa528853a541d9ed00a0b3c8cb033255
Alonso Avotz <alonso@avotz.com>
2017-08-01 11:15:18 -0600

    se agrego patient controller en api

---
+++ app/Http/Controllers/Api/PatientController.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Allergy;
+use App\Http\Requests\PatientRequest;
+use App\Pressure;
+use App\Repositories\PatientRepository;
+use App\Sugar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class PatientController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(PatientRepository $patientRepo)
+    {
+       
+        $this->patientRepo = $patientRepo;
+
+        
+    }
+
+
+
+    /**
+     * Guardar paciente
+     */
+    public function store(PatientRequest $request)
+    {
+        
+        $patient =$this->patientRepo->store($request->all());
+
+
+
+        return $patient;
+
+    }
+
+    /**
+     * Actualizar Paciente
+     */
+    public function update($id, PatientRequest $request)
+    {
+       
+        $patient = $this->patientRepo->update($id, $request->all());
+        
+
+        return $patient;
+
+    }
+
+     /**
+     * Agregar medicamentos a pacientes
+     */
+    public function medicines($id)
+    {
+    
+       $medicine = $this->patientRepo->addMedicine($id, request()->all());
+        
+       return  $medicine;
+
+    }
+
+    /**
+     * Eliminar medicamentos a pacientes
+     */
+    public function deleteMedicines($id)
+    {
+      
+        $medicine = $this->patientRepo->deleteMedicine($id);
+        
+        return '';
+
+    }
+
+     /**
+     * Agregar medicamentos a pacientes
+     */
+    public function allergies($id)
+    {
+        $patient = $this->patientRepo->findById($id);
+        $history = $patient->history;
+
+        $data = request()->all();
+
+        $data['user_id'] = auth()->id();
+
+        $data['history_id'] = $history->id;
+       
+        $allergy = Allergy::create($data);
+        $allergy->load('user.roles');
+     
+        return $allergy;
+    
+   
+
+    }
+
+    /**
+     * Eliminar medicamentos a pacientes
+     */
+    public function deleteAllergies($id)
+    {
+      
+       $allergy = Allergy::findOrFail($id)->delete($id);
+
+     
+      
+        
+        return '';
+
+    }
+
+      /**
+     * Agregar medicamentos a pacientes
+     */
+    public function pressures($id)
+    {
+        $this->validate(request(),[
+                'ps' => 'required|numeric',
+                'pd' => 'required|numeric',
+                'date_control' => 'required',
+                'time_control' => 'required'
+                
+            ]);
+
+        $patient = $this->patientRepo->findById($id);
+        $pressures = $patient->pressures()->create(request()->all());
+
+       
+     
+        return $pressures;
+    
+   
+
+    }
+
+    /**
+     * Eliminar medicamentos a pacientes
+     */
+    public function deletePressures($id)
+    {
+      
+       $allergy = Pressure::findOrFail($id)->delete($id);
+
+     
+      
+        
+        return '';
+
+    }
+
+       /**
+     * Agregar medicamentos a pacientes
+     */
+    public function sugars($id)
+    {
+        $this->validate(request(),[
+                'glicemia' => 'required|numeric',
+                'date_control' => 'required',
+                'time_control' => 'required'
+                
+            ]);
+
+        $patient = $this->patientRepo->findById($id);
+        $sugars = $patient->sugars()->create(request()->all());
+
+       
+     
+        return $sugars;
+    
+   
+
+    }
+
+    /**
+     * Eliminar medicamentos a pacientes
+     */
+    public function deleteSugars($id)
+    {
+      
+       $sugar = Sugar::findOrFail($id)->delete($id);
+
+     
+      
+        
+        return '';
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/token', 'Api\AuthController@token');
 Route::post('/user/social/register', 'Api\AuthController@registerSocial');
 Route::post('/user/register', 'Api\AuthController@register');
+Route::post('/patient/register', 'Api\PatientController@store');
 /*Route::post('/token', function (Request $request) {
 	   
 	    
