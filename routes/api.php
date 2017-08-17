3ad8e4f35b56dce7f81131b8929fe7300b80b118
Alonso Avotz <alonso@avotz.com>
2017-08-17 15:45:08 -0600

    se agrego url reminder api appointments

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::get('/medics/{medic}', 'Api\MedicController@show')->middleware('auth:api'
 Route::get('/medics/{medic}/schedules/list', 'Api\MedicController@getSchedules')->middleware('auth:api');
 Route::get('/medics/{medic}/appointments/list', 'Api\MedicController@getAppointments')->middleware('auth:api');
 Route::post('/appointments', 'Api\AppointmentController@store')->middleware('auth:api');
+Route::post('/appointments/{appointment}/reminder', 'Api\AppointmentController@reminder')->middleware('auth:api');
 /*Route::post('/token', function (Request $request) {
 	   
 	    
