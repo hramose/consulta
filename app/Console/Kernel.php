52d5d1adf71b13a63de4bf0b2693a4345a84181c
Alonso Avotz <alonso@avotz.com>
2017-09-01 15:49:47 -0600

    se puso get par que funcionara en hosgator temporalmente en digital ocea se pone en post

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -59,7 +59,7 @@ class Kernel extends ConsoleKernel
                 curl_close($ch);
                 Log::info('se llamó appointment/reminder: '.$response);
 
-        })->everyMinute();
+        })->everyThirtyMinutes();
 
        /* $schedule->command(\App\Console\Commands\SendPolls::class) //hay q verificar si en el nuevo servidor si funciona asi
                  ->daily();*/
