a8d4d5cc3e923da9f1fe4a82028101640a8d819a
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-10-04 19:14:32 -0600

    se cambio titulo menu ver consultas programadas

--- resources/views/layouts/partials/header-patient.blade.php
+++ resources/views/layouts/partials/header-patient.blade.php
@@ -11,7 +11,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="{{ url('/appointments') }}">Consultas Programadas</a></li>
+            <li><a href="{{ url('/appointments') }}" style="font-weight:bold;">Ver Consultas Programadas</a></li>
           </ul>
         </div>
         <!-- /.navbar-collapse -->
