1390e7d3c8c6f315992879904998a63c809bffb6
Alonso Avotz <alonso@avotz.com>
2017-10-09 16:37:40 -0600

    test delete appointment

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -200,7 +200,7 @@ class AppointmentController extends ApiController
      */
     public function delete($id, Request $request)
     {
-       
+       return request()->user();
         $appointment = $this->appointmentRepo->findById($id);
         $result = 0;
 
