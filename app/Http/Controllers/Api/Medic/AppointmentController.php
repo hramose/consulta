cecb9db3530f549a36a0716258332e7a944b2be5
Alonso Avotz <alonso@avotz.com>
2017-11-20 14:54:37 -0600

    test appointments agrupados

--- app/Http/Controllers/Api/Medic/AppointmentController.php
+++ app/Http/Controllers/Api/Medic/AppointmentController.php
@@ -55,7 +55,7 @@ class AppointmentController extends ApiController
             return $item->id;
         })->toArray();
         
-       dd($grouped);
+      
         $dataAppointments = [];
         $item = [];
 
