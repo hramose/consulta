c326a77feb794c698bd4d7066ea4710957a16c54
Alonso Avotz <alonso@avotz.com>
2017-11-30 12:58:38 -0600

    se corrgio update desde el perfil medico telefonos duplicados

--- app/Http/Controllers/Medic/PatientController.php
+++ app/Http/Controllers/Medic/PatientController.php
@@ -160,7 +160,7 @@ class PatientController extends Controller
                 //'address' => 'required',
                 'province' => 'required',
                 //'city' => 'required',
-                'phone' => 'required',
+                'phone' => ['required',Rule::unique('patients')->ignore($id)],
                 'email' => ['email', Rule::unique('patients')->ignore($id)]//'required|email|max:255|unique:patients',
         ]);
 
@@ -173,10 +173,12 @@ class PatientController extends Controller
         if($user_patient){
 
             $this->validate(request(),[ //se valida que no exista en user el correo q quiere cambiar
+                    'phone' => ['required', Rule::unique('users')->ignore($user_patient->id)]
                     'email' => ['email', Rule::unique('users')->ignore($user_patient->id)]
             ]);
         }else{
             $this->validate(request(),[ //se valida que no exista en user el correo q quiere cambiar
+                    'phone' => ['required', Rule::unique('users')->ignore($user_patient->id)]
                     'email' => ['email', Rule::unique('users')]
             ]);
         }
