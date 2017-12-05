9f1c3168560c6012de912a597ef7c7cf156e2433
Alonso Avotz <alonso@avotz.com>
2017-12-05 17:16:47 -0600

    test meessage pus only data

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -109,13 +109,18 @@ class AppointmentController extends Controller
         if($medic->push_token){
                 $push = new PushNotification('fcm');
                 $response = $push->setMessage([
+                       'title'=>'Nueva Cita Reservada',
+                       'body'=>'Para el '.  Carbon::parse($appointment->start)->toDateTimeString(),
+                       'badge' => '1'
+                   ]/*[
                     'notification' => [
                             'title'=>'Nueva Cita Reservada',
                             'body'=>'Para el '.  Carbon::parse($appointment->start)->toDateTimeString(),
                             'sound' => 'default'
                             ]
                     
-                    ])
+                    ]*/)
+
                     ->setApiKey(env('API_WEB_KEY_FIREBASE_MEDICS'))
                     ->setDevicesToken($medic->push_token)
                     ->send()
