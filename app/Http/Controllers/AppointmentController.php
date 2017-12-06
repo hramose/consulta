490ebed649293e7bb1c9ed8cada24fdaa150fa3e
Alonso Avotz <alonso@avotz.com>
2017-12-05 19:26:02 -0600

    test meessage pus only data

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -108,7 +108,7 @@ class AppointmentController extends Controller
 
         if($medic->push_token){
                 $push = new PushNotification('fcm');
-                $response = $push->setMessage([
+                /*$response = $push->setMessage([
                        'title'=>'Nueva Cita Reservada',
                        'body'=>'Para el '.  Carbon::parse($appointment->start)->toDateTimeString(),
                        'badge' => '1'
@@ -118,20 +118,20 @@ class AppointmentController extends Controller
                     ->getFeedback();
                     
                     Log::info('Mensaje Push code: '.$response->success);
+                */
+                $response = $push->setMessage([
+                    'notification' => [
+                            'title'=>'Nueva Cita Reservada',
+                            'body'=>'Para el '.  Carbon::parse($appointment->start)->toDateTimeString(),
+                            'sound' => 'default'
+                            ]
                     
-                // $response = $push->setMessage([
-                //     'notification' => [
-                //             'title'=>'Nueva Cita Reservada',
-                //             'body'=>'Para el '.  Carbon::parse($appointment->start)->toDateTimeString(),
-                //             'sound' => 'default'
-                //             ]
+                    ])->setApiKey(env('API_WEB_KEY_FIREBASE_MEDICS'))
+                    ->setDevicesToken($medic->push_token)
+                    ->send()
+                    ->getFeedback();
                     
-                //     ])->setApiKey(env('API_WEB_KEY_FIREBASE_MEDICS'))
-                //     ->setDevicesToken($medic->push_token)
-                //     ->send()
-                //     ->getFeedback();
-                    
-                //     Log::info('Mensaje Push code: '.$response->success);
+                    Log::info('Mensaje Push code: '.$response->success);
            }
         
        // $appointmentToPusher = Appointment::with('patient','user')->find($appointment->id);
