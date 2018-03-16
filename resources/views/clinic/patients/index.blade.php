381c6b91abb9c8e75d0085d2c9de2e97281724e0
Alonso Avotz <alonso@avotz.com>
2018-03-16 17:04:09 -0600

    se acomodo el titulo y body del anuncio de primero

--- resources/views/clinic/patients/index.blade.php
+++ resources/views/clinic/patients/index.blade.php
@@ -121,25 +121,8 @@
                       <b>Marketing:</b> <small>Selecciona los paciente que deseas enviarles información</small>
                     </div>
                     <div class="panel-body">
-                        <div class="marketing">
-                          
-                              <div class="marketing-file">
-                                  
-                                      <input type="file" name="file" id="file" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" />
-                                      <label for="file"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span>Escoger archivo&hellip;</span></label>
-                                    
-                                  
-                                
-                                </div>
-                                <div class="button-file-send">
-                                        
-                                      <button type="submit" class="btn-multiple btn btn-danger btn-sm btn-flat btn-block" data-action="send-marketing" title="Enviar" form="send-marketing" formaction="/patients/marketing"><i class="fa fa-send"></i> Enviar anuncio </button>
-                                  
-                                
-                                </div>
                         
-                        </div>
-                        <div class="marketing-info">
+                        <div class="marketing-info row">
                             <div class="form-group">
                            
                               <div class="col-sm-3">
@@ -162,7 +145,31 @@
                                     
                                   
                               </div>
+                            </div>
                         </div>
+                        <div></div>
+                        <div class="marketing row">
+                           <div class="col-sm-12">
+                              <div class="marketing-flex">
+                                  <div class="marketing-file">
+                                    
+                                        <input type="file" name="file" id="file" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" />
+                                        <label for="file"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span>Escoger archivo&hellip;</span></label>
+                                      
+                                    
+                                  
+                                  </div>
+                                  <div class="button-file-send">
+                                          
+                                        <button type="submit" class="btn-multiple btn btn-danger btn-sm btn-flat btn-block" data-action="send-marketing" title="Enviar" form="send-marketing" formaction="/patients/marketing"><i class="fa fa-send"></i> Enviar anuncio </button>
+                                    
+                                  
+                                  </div>
+                              </div>
+                              
+                            </div>
+                        </div>
+                        
 
 
                     </div>
